5085cbb031015a4c81c2cabd88c24c7d968195e5
Prashanth Pujar <prashanth.pujar.usc@gmail.com>
2025-04-12 08:19:26 -0700

    Linked List using CudaMallocManaged
    **Description**
    
    Print elements of linked in GPU using CudaMallocManaged
    
    **Testing**
    
    nvcc -o linked_list linked_list.cu
    
    From device: [cpu]
    key = 0
    key = 1
    key = 2
    From device: [gpu]
    key = 0
    key = 1
    key = 2

--- exercises/hw6/linked_list.cu
+++ exercises/hw6/linked_list.cu
@@ -20,34 +20,39 @@ struct list_elem {
 
 template <typename T>
 void alloc_bytes(T &ptr, size_t num_bytes){
-
-  ptr = (T)malloc(num_bytes);
+  cudaMallocManaged(&ptr, num_bytes);
+  //ptr = (T)malloc(num_bytes);
 }
 
 __host__ __device__
-void print_element(list_elem *list, int ele_num){
+void print_element(list_elem *list, int ele_num, char* device){
   list_elem *elem = list;
-  for (int i = 0; i < ele_num; i++)
+  printf("From device: %s\n",  device);
+  for (int i = 0; i < ele_num; i++) {
+    printf("key = %d\n", elem->key);
     elem = elem->next;
-  printf("key = %d\n", elem->key);
+   }
 }
 
 __global__ void gpu_print_element(list_elem *list, int ele_num){
-  print_element(list, ele_num);
+    char device[] = "[gpu]";
+    print_element(list, ele_num, device);
 }
 
 const int num_elem = 5;
 const int ele = 3;
 int main(){
-
+  // list_base = ptr to head node
   list_elem *list_base, *list;
   alloc_bytes(list_base, sizeof(list_elem));
   list = list_base;
   for (int i = 0; i < num_elem; i++){
     list->key = i;
     alloc_bytes(list->next, sizeof(list_elem));
-    list = list->next;}
-  print_element(list_base, ele);
+    list = list->next;
+  }
+  char device[] = "[cpu]";
+  print_element(list_base, ele, device);
   gpu_print_element<<<1,1>>>(list_base, ele);
   cudaDeviceSynchronize();
   cudaCheckErrors("cuda error!");
